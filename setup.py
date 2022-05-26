2b7f630f9f515402e83810c4dde1189b8b346d97
mkmer <mike.j.kasper@gmail.com>
2022-05-25 20:51:29 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='AIOAladdinConnect',
+    version='0.1',
+    author='Mike Kasper',
+    author_email='m_kasper@sbcglobal.net',
+    url='http://github.com/mkmer/AIOAladdinConnect',
+    packages=['AIOAladdinConnect'],
+    scripts=[],
+    description='Python Async API for controlling Genie garage doors connected to Aladdin Connect devices',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires=['aiohttp'],
+    include_package_data=True
+)
