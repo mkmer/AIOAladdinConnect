bc4952b7e3e46f5109c00dba9d092c4a0433d780
mkmer <mike.j.kasper@gmail.com>
2022-07-30 10:49:11 -0400

    cancelled task wait

--- AIOAladdinConnect/eventsocket.py
+++ AIOAladdinConnect/eventsocket.py
@@ -93,4 +93,5 @@ class EventSocket:
         await self._websocket.close()
         self._websocket = None
         self._run_future.cancel()
+        await self._run_future.cancelled()
         self._run_future = None
