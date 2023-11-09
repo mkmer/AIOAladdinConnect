b557ab5c2e88b8ccec6411a112abd900163a62b4
mkmer <m_kasper@sbcglobal.net>
2023-11-09 12:04:18 -0500

    Add auth_mgr

---
+++ AIOAladdinConnect/auth_mgr.py
@@ -0,0 +1,27 @@
+"""Aladdin Connect authorization manager."""
+import abc
+from aiohttp import ClientSession
+
+
+class AladdinAuthMgr(metaclass=abc.ABCMeta):
+    """Aladdin Connect API Authentication Manager."""
+
+    def __init__(self, session: ClientSession) -> None:
+        """YoLink Auth Manager"""
+        self._session = session
+
+    def client_session(self) -> ClientSession:
+        """Get client session."""
+        return self._session
+
+    @abc.abstractmethod
+    def access_token(self) -> str:
+        """Get auth token."""
+
+    def http_auth_header(self) -> str:
+        """Get auth header."""
+        return f"Bearer {self.access_token()}"
+
+    @abc.abstractmethod
+    async def check_and_refresh_token(self) -> str:
+        """Check and fresh token."""
