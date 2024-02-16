22290841a894c20a26d2d46d52d47ae21fe13e80
mkmer <m_kasper@sbcglobal.net>
2024-02-16 11:30:32 -0500

    First updates to headers and login

--- AIOAladdinConnect/session_manager.py
+++ AIOAladdinConnect/session_manager.py
@@ -1,9 +1,12 @@
 """Aladdin Connect Session Manager."""
 import base64
 import logging
+import hmac
+import hashlib
+import uuid
 import socket
 import aiohttp
-from .const import _LOGGER
+from .const import _LOGGER, CLIENT_ID, CLIENT_SECRET
 
 class SessionManager:
     """A session Manager for Aladdin Connect."""
@@ -12,7 +15,8 @@ class SessionManager:
     # API_BASE_URL = "https://16375mc41i.execute-api.us-east-1.amazonaws.com/IOS"
     RPC_URL = API_BASE_URL
 
-    LOGIN_ENDPOINT = "/oauth/token"
+    #LOGIN_ENDPOINT = "/oauth/token"
+    LOGIN_ENDPOINT = "https://cognito-idp.us-east-2.amazonaws.com/"
     LOGOUT_ENDPOINT = "/session/logout"
     X_API_KEY = "fkowarQ0dX9Gj1cbB9Xkx1yXZkd6bzVn5x24sECW"  # Android
     # X_API_KEY = "2BcHhgzjAa58BXkpbYM977jFvr3pJUhH52nflMuS" # IOS
@@ -44,27 +48,44 @@ class SessionManager:
         """Login to Aladdin Connect service."""
         self._auth_token = None
         self._logged_in = False
-        password_base64 = base64.b64encode(self._password.encode("utf-8")).decode(
-            "utf-8"
-        )
-        payload = {
-            "grant_type": "password",
-            "client_id": self._client_id,
-            "brand": "ALADDIN",
-            "username": self._user_email,
-            "password": password_base64,
-            "app_version": "5.30",
-            # "platform": "platform",
-            # "model": "Google Pixel 6",
-            # "build_number": "2042",
-            # "os_version": "12.0.0"
-        }
 
-        url = self.API_BASE_URL + self.LOGIN_ENDPOINT
+        def get_secret_hash(username):
+            """Get the secret hash."""
+            msg = username + CLIENT_ID
+            dig = hmac.new(
+                str(CLIENT_SECRET).encode('utf-8'),
+                msg = str(msg).encode('utf-8'),
+                digestmod=hashlib.sha256).digest()
+            d2 = base64.b64encode(dig).decode()
+            return d2
+
+        payload = {
+            "AuthFlow":"USER_PASSWORD_AUTH",
+            "AuthParameters":{
+                #"SECRET_HASH":"1lSy+LXf8zSVmq3GMU2ixqlLT/36rlD0eW4nQOWuctc=",
+                "SECRET_HASH":get_secret_hash(self._user_email),
+                "PASSWORD":self._password,
+                "USERNAME":self._user_email,
+                },
+            #"ClientMetadata":{},
+            "ClientId":CLIENT_ID,
+            }
+
+
+        url = self.LOGIN_ENDPOINT
         _LOGGER.debug("Sending payload: %s", payload)
+        headers = {
+                    "Content-Type": "application/x-amz-json-1.1",
+                    "Accept-Encoding": "identity",
+                    "aws-sdk-invocation-id": "c4e03fa8-a542-4079-b19b-28c3b6e9be63",
+                    "Connection": "Keep-Alive",
+                    "Host": "cognito-idp.us-east-2.amazonaws.com",
+                    "User-Agent": "amplify-android/1.37.3 (Android 12; Google Pixel 3; en_US)",
+                    "X-Amz-Target" : "AWSCognitoIdentityProviderService.InitiateAuth"
+                }
         try:
             response = await self._session.post(
-                url, data=payload, headers=self._headers
+                url, data=payload, headers=headers
             )
             _LOGGER.debug("Received Response: %s", response)
             if response.status == 401:
